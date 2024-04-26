68b2b2899d1e9062021110011e162496184e69b0
Eyüp Erdoğan <epsilonelittr@gmail.com>
2024-04-26 02:17:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Vite + Lit + TS + SASS + TW Starter Template
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["src/**/*.{ts,scss}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
